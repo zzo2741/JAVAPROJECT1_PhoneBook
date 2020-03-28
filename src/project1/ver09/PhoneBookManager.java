06cac39f8cc4fec82b83486697da0ca04344be44
dlwnstmd@gmail.com <a35243467>
2020-03-28 15:40:19 +0900

    0328

--- src/project1/ver09/PhoneBookManager.java
+++ src/project1/ver09/PhoneBookManager.java
@@ -180,8 +180,7 @@ public class PhoneBookManager extends ConnectImpl
 
 	public void createSeq()
 	{
-		try
-		{
+		try/		{
 			stmt = con.createStatement();
 			String createSeqSql = "create sequence seq_phonebook nocycle nocache";
 			stmt.execute(createSeqSql);
